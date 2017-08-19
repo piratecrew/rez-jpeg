028230c859c7dc0201dbb94e20633880e42b7012
Fredrik Brännbacka FL <fredrik.brannbacka@ilpvfx.com>
2017-08-19 16:03:31 +0200

    Added nasm to travis conf

--- package.py
+++ package.py
@@ -9,5 +9,7 @@ authors = ['fredrik.brannbacka']
 variants = [["platform-linux"]]
 
 def commands():
+    env.PATH.prepend("{root}/bin")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
     if building:
         env.CMAKE_MODULE_PATH.append("{root}/cmake")
